2e58d6063ec2c3ab48b29060549d03e0c182d94b
naveen2u4 <naveen2u4@hotmail.com>
2016-10-02 10:46:58 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/artical-one', function (req, res) {
-  res.send(createartical(htmlTemplate));
+  res.send(createTemplate(htmlTemplate));
   //res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 app.get('/artical-two', function (req, res) {
