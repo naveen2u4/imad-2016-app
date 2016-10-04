aa08303c2f9193ab4effa4920e90c4c7f7c98fe5
naveen2u4 <naveen2u4@hotmail.com>
2016-10-04 07:24:14 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 //Chane the main text
-var element = docement.GetElementById('main-text');
+var element = document.GetElementById('main-text');
 element.innerHTML = 'NAVEEN';
