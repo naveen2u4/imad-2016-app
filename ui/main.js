fa73f3dd9c63ab96a7b64110188f87d321ad477a
naveen2u4 <naveen2u4@hotmail.com>
2016-10-04 07:42:16 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,15 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'NAVEEN';
 //Move the image
 var img = document.getElementById('madi');
+
+
+var marginLeft = 0;
+function(){
+    marginLeft = marginLeft+10;
+    img.style.margin = 'marginLeft'+ 'px';
+}
 img.onclick = function(){
+    var interval = setInterval(moveReightt,100);
     img.style.marginLeft = '100px';
     
     
