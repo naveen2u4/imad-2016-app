9a24f107fabaf68b75f467d6599af6e45c585f4f
naveen2u4 <naveen2u4@hotmail.com>
2016-10-04 07:49:19 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ function moveRight(){
     img.style.margin = 'marginLeft'+ 'px';
 }
 img.onclick = function(){
-    var interval = setInterval(moveReight,100);
+    var interval = setInterval(moveRight,100);
     img.style.marginLeft = '100px';
     
     
